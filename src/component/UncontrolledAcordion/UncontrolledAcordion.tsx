f0b00772939d235fafb84adb9386e3f6333c090f
makstravm <maks7-2-94@gmail.com>
2021-06-20 21:18:53 +0300

    study hook useReduce, added useReduce Uncontrolled Accordion, added test reduce

--- src/component/UncontrolledAcordion/UncontrolledAcordion.tsx
+++ src/component/UncontrolledAcordion/UncontrolledAcordion.tsx
@@ -1,21 +1,25 @@
 import { type } from 'os';
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import style from '../button.module.css';
+import { reducer, TOGGLE__CONSTANT } from './reducer';
 
 type AcordionPropsType = {
   title: string
 
 }
 
+
 export function UncontrolledAcordion(props: AcordionPropsType) {
 
-  const [collapse, setCollapse] = useState(true)
+  // const [collapse, setCollapse] = useState(true)
+  const [state, dispatch] = useReducer(reducer, {collapsed : false})
 
   return (
     <div className={style.wrapper}>
-      <AcordionTitle title={props.title} onClick={() => { setCollapse(!collapse) }} />
+      {/* <AcordionTitle title={props.title} onClick={() => { setCollapse(!collapse) }} /> */}
+      <AcordionTitle title={props.title} onClick={() => { dispatch({ type:TOGGLE__CONSTANT}) }} />
 
-      { !collapse && <AcordionBody />}
+      {!state.collapsed && <AcordionBody />}
     </div >
   )
 }
